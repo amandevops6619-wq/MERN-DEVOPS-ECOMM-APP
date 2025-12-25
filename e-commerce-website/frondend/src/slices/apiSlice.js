933a59cfc7022164c0a67e9a0986e52b98aca10a
Aman Gupta <aman.devops6619@gmail.com>
2025-12-25 13:06:24 +0000

    Fixed bakcend url

--- e-commerce-website/frondend/src/slices/apiSlice.js
+++ e-commerce-website/frondend/src/slices/apiSlice.js
@@ -1,8 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: import.meta.env.VITE_PRODUCT_API || "" })
-
-console.log("API URL:", import.meta.env.VITE_PRODUCT_API);
+const baseQuery = fetchBaseQuery({ baseUrl: "" })
 
 export const apiSlice = createApi({
     baseQuery,
